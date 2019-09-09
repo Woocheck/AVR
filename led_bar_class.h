536995b0861bc03e215b6b65f0d45186fea02cdb
Woocheck <34909593+Woocheck@users.noreply.github.com>
2019-09-09 21:19:45 +0200

    Update led_bar_class.h

--- led_bar_class.h
+++ led_bar_class.h
@@ -15,23 +15,17 @@
 
 class led_bar_class
 {
-//variables
-public:
-protected:
-private:
-uint8_t proc;
-//functions
-public:
+	uint8_t proc;
+    public:
 	led_bar_class();
 	~led_bar_class();
 	void init();
 	void procent_display(uint8_t procent);
-protected:
-private:
+    private:
 	led_bar_class( const led_bar_class &c );
 	led_bar_class& operator=( const led_bar_class &c );
 	
 
-}; //led_bar_class
+};
 
 #endif //__LED_BAR_CLASS_H__
